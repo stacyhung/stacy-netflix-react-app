9855d95b53b689b0248b904aa398c54a4962b5a2
Stacy Hung <sshung@gmail.com>
2021-01-05 10:51:35 -0800

    style jumbotron

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from "react";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Home from './pages/home';
 
 function App() {
     return (
-        <h1>Hello World!</h1>
+        <Router>
+            <Switch>
+                <Route path="/signin">
+                    <p>Sign-in page</p>
+                </Route>
+                <Route path="/signup">
+                    <p>Sign-up page</p>
+                </Route>
+                <Route path="/browse">
+                    <p>Browse page</p>
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
+        // <h1>Hello World - am I still working?</h1>
     )
 }
 
