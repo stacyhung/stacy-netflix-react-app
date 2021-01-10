dc0048799daf523e2e2e7c0eecaa0cab4ccc6458
Stacy Hung <sshung@gmail.com>
2021-01-09 22:13:47 -0800

    build accordian

---
+++ src/components/accordion/index.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { Body, Title, Header, Container, Inner, Item } from "./styles/accordian";
+
+export default function Accordian({ children, ...restProps }) {
+    return (
+        <Container {...restProps} >
+            <Inner>{children}</Inner>
+        </Container>
+    )
+}
+
+Accordian.Item = function AccordianItem({children, ...restProps}) {
+    return <Item {...restProps}>{children}</Item>;
+}
+
+Accordian.Title = function AccordianTitle({children, ...restProps}) {
+    return <Title {...restProps}>{children}</Title>;
+}
+
+Accordian.Header = function AccordianHeader({children, ...restProps}) {
+    return <Header {...restProps}>{children}</Header>;
+}
+
+Accordian.Body = function AccordianBody({children, ...restProps}) {
+    return <Body {...restProps}>{children}</Body>;
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Jumbotron } from "./jumbotron"; // component library tree shaking
 export { default as Footer } from "./footer";
+export { default as Accordian } from "./accordion";
