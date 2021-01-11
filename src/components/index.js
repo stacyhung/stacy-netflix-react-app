9c0f35008d96dc0c7bed4c2d38e63b13d42663fb
Stacy Hung <sshung@gmail.com>
2021-01-11 09:07:23 -0800

    build opt-form component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Jumbotron } from "./jumbotron"; // component library tree shaking
 export { default as Footer } from "./footer";
 export { default as Accordian } from "./accordion";
+export { default as OptForm } from "./opt-form";
--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,20 +1,24 @@
 import React from "react";
-import { Accordian } from "../components"
+import { Accordian } from "../components";
+import OptForm from "../components/opt-form";
 import faqsData from "../fixtures/faqs.json";
 
 export function FaqsContainer() {
-    return (
-        <Accordian>
-            <Accordian.Title>Frequently Asked Questions</Accordian.Title>
-            <Accordian.Frame>
-            {faqsData.map((item) => (
-                <Accordian.Item key={item.id}>
-                    <Accordian.Header>{item.header}</Accordian.Header>
-                    <Accordian.Body>{item.body}</Accordian.Body>
-                </Accordian.Item>
+  return (
+    <Accordian>
+      <Accordian.Title>Frequently Asked Questions</Accordian.Title>
+      <Accordian.Frame>
+        {faqsData.map((item) => (
+          <Accordian.Item key={item.id}>
+            <Accordian.Header>{item.header}</Accordian.Header>
+            <Accordian.Body>{item.body}</Accordian.Body>
+          </Accordian.Item>
+        ))}
+      </Accordian.Frame>
 
-            ))}
-            </Accordian.Frame>
-        </Accordian>
-    )
+      <OptForm>
+        <OptForm.Input placeholder="Email address" />
+      </OptForm>
+    </Accordian>
+  );
 }
