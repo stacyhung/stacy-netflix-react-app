df413af4cb28f6c5e1dd08e06629665541ef4652
Stacy Hung <sshung@gmail.com>
2021-02-25 09:14:06 -0800

    build form component and sign-in page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
-import Home from "./pages/home";
+import { Home, Signin } from "./pages";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path={ROUTES.SIGN_IN}>
-          <p>Sign-in page</p>
+          <Signin />
         </Route>
         <Route path={ROUTES.SIGN_UP}>
           <p>Sign-up page</p>
