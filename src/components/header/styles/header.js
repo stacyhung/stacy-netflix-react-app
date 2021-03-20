1b2d7864d794d150eb2e4c47d6508a607558a42a
Stacy Hung <sshung@gmail.com>
2021-03-20 11:49:12 -0700

    add feature to header component

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -4,7 +4,10 @@ import { Link as ReachRouterLink } from "react-router-dom";
 export const Background = styled.section`
   display: flex;
   flex-direction: column;
-  background: url("../images/misc/home-bg.jpg") top left / cover no-repeat;
+  //background: url("../images/misc/home-bg.jpg") top left / cover no-repeat;
+  background: url(${({ src }) =>
+      src ? `/images/misc/${src}.jpg` : `/images/misc/home-bg.jpg`})
+    top left / cover no-repeat;
 
   @media (max-width: 1100px) {
     ${({ dontShowOnSmallViewPort }) =>
@@ -72,4 +75,16 @@ export const Logo = styled.img`
     height: 45px;
     width: 167px;
   }
+`;
+
+export const Group = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Text = styled.p`
+  color: white;
+  font-size: 22px;
+  line-height: normal;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
 `;
